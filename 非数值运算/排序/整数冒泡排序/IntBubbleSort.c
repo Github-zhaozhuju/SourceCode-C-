89ab9c5c540b3acd03f038b0ee43b96cae8f1ef7
Github-zhaozhuju <zhaozhuju666@gmail.com>
2020-09-06 23:10:16 +0800

    add IntBubbleSort Notee

--- 非数值运算/排序/整数冒泡排序/IntBubbleSort.c
+++ 非数值运算/排序/整数冒泡排序/IntBubbleSort.c
@@ -1,5 +1,40 @@
 #include "stdio.h"
-
+/*
+两个数比较可以用中间变量替换或者位运算
+*/
+void ibub(int *p ,int n)
+{
+	int m,k,j,i,d;
+	k=0;
+	m=n-1;
+	while(k<m)
+	{
+		j=m-1;
+		m=0;
+		for(i=k;i<=j;i++)
+			if(p[i]>p[i+1])
+			{
+				d=p[i];
+				p[i]=p[i+1];
+				p[i+1]=d;
+				m=i;
+			}
+		j=k+1;
+		k=0;
+		for(i=m;i>=j;i--)
+			if(p[i-1]>p[i])
+			{
+				d=p[i];
+				p[i]=p[i-1];
+				p[i-1]=d;
+				k=i;
+			}
+		
+	}
+}
+/*
+利用位运算时需注意，如果两个数相等则不能使用位运算
+*/
 void RubbleSort(int array[], size_t size)
 {
 	size_t i, j;
@@ -13,7 +48,9 @@ void RubbleSort(int array[], size_t size)
 			}
 }
 
-
+/*
+冒泡排序时间复杂度为 O(n2)
+*/
 void main()
 {
 	int count=0;
@@ -23,9 +60,11 @@ void main()
 	for(count=0;count<10;count++)
 		printf("%d\t",a[count]);
 		
-	RubbleSort(a,10);
+	ibub(a,10);
 	
 	printf("\n排序之后：\n");
 	for(count=0;count<10;count++)
 	 	printf("%d\t",a[count]);
+	 	
+	while(1); 
 } 
